370ebbc6e9678c0abca63890596f66b995246216
jjjjjjpppppp <jonphey91@gmail.com>
2023-10-20 17:02:30 +1100

    Added a Card class with number and operator attributes. Imported to main.py and added number and operator variables. Ref JON-37

---
+++ card.py
@@ -0,0 +1,4 @@
+class Card:
+    def __init__(self, number, operator): # always need to initialise the class
+        self.number = number # 
+        self.operator = operator
---
+++ main.py
@@ -0,0 +1,4 @@
+import card
+
+number = list(range(11)) # Creates a list from 0-10, i.e. number = [0, 1, 2, 3, ...]
+operator = ['+', '-', '×', '÷']
