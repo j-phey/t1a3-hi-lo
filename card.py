5b2a9142b6c76cca4c734e0d0a65ac3f05d263aa
jjjjjjpppppp <jonphey91@gmail.com>
2023-10-23 17:52:44 +1100

    Created function for deal_cards() and variable for player's hand

--- card.py
+++ card.py
@@ -1,4 +1,4 @@
 class Card:
-    def __init__(self, value, card_type): # always need to initialise the class
-        self.value = value  
-        self.card_type = card_type
+    def __init__(self, value, card_type): # always need to initialise the class first!
+        self.value = value  # Holds the value of the card, e.g. 1, 2, 3 or - or +
+        self.card_type = card_type # Holds the type of card, i.e. number or operator
--- main.py
+++ main.py
@@ -1,9 +1,8 @@
+# --- IMPORT CLASSES, MODULES AND PACKAGES ---
 import card # Importing card class from card.py
 import random # Importing in-built Random Python module for shuffling the cards
 
-# (Decided to turn this into a function)
-# number = list(range(11)) # Creates a list from 0-10, i.e. number = [0, 1, 2, 3, ...]
-# operator = ['+', '-', '×', '÷']
+# --- FUNCTIONS ---
 
 # Function for creating the initial deck of cards
 def deck(): 
@@ -14,14 +13,44 @@ def deck():
 # Function for shuffling the deck of cards
 def shuffle_deck(cards_to_shuffle): 
     random.shuffle(cards_to_shuffle)
+    return cards_to_shuffle
+
+# Function for dealing 4 number cards and 3 operator cards
+def deal_cards(player_hand, shuffled_deck):
+    num_number_cards = 0 # Sets the initial amount of number cards 
+    num_operator_cards = 0 # Sets the initial amount of operator cards 
+
+    for card in shuffled_deck: # Start iterating through the shuffled deck
+        if card.card_type == 'number' and num_number_cards < 4: # If there are less than 5 number cards that have been dealt
+            player_hand.append(card) # Put it in the player_hand list
+            num_number_cards += 1 
+        elif card.card_type == 'operator' and num_operator_cards < 3: # If there are less than 4 operator cards that have been dealt
+            player_hand.append(card)
+            num_operator_cards += 1
+        
+        if num_number_cards == 4 and num_operator_cards == 3: # Once 4 numbers and 3 operator cards have been dealt, break out of the loop
+            break 
+    
+    for card in player_hand:
+        shuffled_deck.remove(card)
+
+# --- CREATE DECK OF CARDS ---
 
 # Create a deck of cards using the defined lists in the deck() function
 number_cards, operator_cards = deck() 
 deck_of_cards = number_cards + operator_cards
 
 # Create a shuffled deck of cards using the shuffle_deck() function
-shuffle_deck(deck_of_cards) 
+shuffled_deck = shuffle_deck(deck_of_cards) 
 
+# --- DEAL THE CARDS ---
+
+player_hand = [] # Creates an empty hand or list for the player
+deal_cards(player_hand, shuffled_deck) # Deals the cards from the deal_cards function
+
+
+
+# ------- Print statements to test output ---------
 print('Number cards:')
 for card in number_cards:
     print(f'{card.value} ', end=' ') # Using 'end' to print across a horizontal line
@@ -32,4 +61,12 @@ for card in operator_cards:
 
 print(f'\n\nShuffled deck:')
 for card in deck_of_cards:
+    print(f'{card.value} ', end=' ')
+
+print(f'\n\nPlayers hand:')
+for card in player_hand:
+    print(f'{card.value} ', end=' ')
+
+print(f'\n\nRemaining cards in deck:')
+for card in shuffled_deck:
     print(f'{card.value} ', end=' ')
