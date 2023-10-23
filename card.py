b8f08fcbf62fe86c95134a3ff28268133fc7f424
jjjjjjpppppp <jonphey91@gmail.com>
2023-10-23 14:12:54 +1100

    Created the deck of cards with a deck() function

--- card.py
+++ card.py
@@ -1,6 +1,4 @@
 class Card:
-    def __init__(self, number, operator): # always need to initialise the class
-        self.number = number # 
-        self.operator = operator
-
-
+    def __init__(self, value, card_type): # always need to initialise the class
+        self.value = value  
+        self.card_type = card_type
--- main.py
+++ main.py
@@ -1,4 +1,19 @@
 import card
 
-number = list(range(11)) # Creates a list from 0-10, i.e. number = [0, 1, 2, 3, ...]
-operator = ['+', '-', '×', '÷']
+# number = list(range(11)) # Creates a list from 0-10, i.e. number = [0, 1, 2, 3, ...]
+# operator = ['+', '-', '×', '÷']
+
+def deck(): # Creates the deck of cards as a function
+    number_cards = [card.Card(value, 'number') for value in range(11)]  # Creates a list from 0-10, i.e. number = [0, 1, 2, 3, ...]. Used to identify number cards as 'number'
+    operator_cards = [card.Card(operator, 'operator') for operator in ['+', '-', '×', '÷']]
+    return number_cards, operator_cards
+
+number_cards, operator_cards = deck()
+
+print('Number cards:')
+for card in number_cards:
+    print(f'{card.value} {card.card_type}')
+
+print('Operator cards:')
+for card in operator_cards:
+    print(f'{card.value} {card.card_type}')
